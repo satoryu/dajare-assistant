5013a0dd7292f9646cfb75c0e4bb410af5375c54
Tatsuya Sato <satoryu.1981@gmail.com>
2019-07-27 20:55:14 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const {dialogflow} = require('actions-on-google')
+const request = require('request-promise-native')
+
+const app = dialogflow({debug: true})
+
+const fetch = async function(text) {
+    const options = {
+        json: true,
+        body: {
+            text: text
+        }
+    }
+    try {
+        const response = await request.post(functions.config().dajare_api.url, options);
+
+        return response['puns'];
+    } catch(err) {
+        throw err;
+    }
+};
+
+app.intent('Default Welcome Intent', (conv) => {
+    conv.ask(`<speak>こんにちわ、駄洒落ボットです。何か喋ってください。</speak>`)
+});
+
+app.intent('Default Fallback Intent', async (conv) => {
+    const puns = await fetch(conv.input.raw)
+
+    conv.ask(`<speak>${puns[0]}</speak>`)
+    conv.ask(`<speak>${conv.input.raw}</speak>`)
+})
+
+express().use(bodyParser.json(), app).listen(process.env.PORT || 3000)
